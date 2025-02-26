d8cf6f18d56f18556b6c150d04b7238b2b008e3f
GOSIjnr <awugosi7@gmail.com>
2025-02-26 05:55:21 +0100

    import v2
    - import sound files

---
+++ NewScript.cs
@@ -0,0 +1,16 @@
+using Godot;
+
+namespace GOSIjnr;
+
+public partial class NewScript : Node
+{
+	public override void _Ready()
+	{
+
+	}
+
+	public override void _Process(double delta)
+	{
+
+	}
+}
